c31cf3839f6d07559f65d3cd2eed6b9868492bde
filipskrabak <xskrabakf@stuba.sk>
2026-03-16 00:16:00 +0100

    feat(project.R): cleanup the code, filter years, filter station, data analysis

--- project.R
+++ project.R
@@ -1,10 +1,18 @@
 library(tidyverse)
 library(arrow)
 library(lubridate)
-library(here)
+library(here) # for handling paths correctly
 
-clean_air_data <- open_dataset(here("pollutants")) %>%
-  select(Start, Pollutant, Value, Samplingpoint) %>% # lazy loading
+# Description of this dataset is in docs/POLLUTANTS.md
+pollutants_data <- open_dataset(here("pollutants"))
+
+glimpse(pollutants_data)
+# TODO: check variables, such as Validity, or AggType.
+
+clean_air_data <- pollutants_data %>%
+  select(Start, Pollutant, Value, Samplingpoint, Validity) %>% # lazy loading
+  filter(grepl("SK0001A", Samplingpoint)) %>%
+  filter(year(Start) >= 2017, year(Start) <= 2024) %>%
   collect() %>%
   # Translate EU codes into factors
   mutate(Pollutant_Name = case_when(
@@ -18,7 +26,6 @@ clean_air_data <- open_dataset(here("pollutants")) %>%
   )) %>%
   select(Start, Pollutant_Name, Value) %>%
   group_by(Start, Pollutant_Name) %>%
-  # This takes the average of multiple stations! TODO: fix
   summarise(Value = mean(Value, na.rm = TRUE), .groups = "drop") %>%
   pivot_wider(
     names_from = Pollutant_Name, 
@@ -30,10 +37,14 @@ glimpse(clean_air_data)
 
 View(clean_air_data)
 
+sum(complete.cases(clean_air_data)) # 60845
+nrow(clean_air_data) # 69718
+
 ## Weather
 
 weather_file_path <- here("weather", "open-meteo-48.12N17.10E163m.csv")
 
+# TODO: select more variables, reason about which ones
 weather_data <- read_csv(weather_file_path, skip = 3) %>%
   rename(
     Start = time,
@@ -41,11 +52,13 @@ weather_data <- read_csv(weather_file_path, skip = 3) %>%
     Rain = `rain (mm)`,
     Wind_Speed = `wind_speed_10m (km/h)`
   ) %>%
-  select(Start, Temperature, Rain, Wind_Speed)
+  select(Start, Temperature, Rain, Wind_Speed) %>%
+  filter(year(Start) >= 2017, year(Start) <= 2024)
 
 complete_data <- clean_air_data %>%
-  inner_join(weather_data, by = "Start") %>%
-  fill(where(is.numeric), .direction = "down")
+  inner_join(weather_data, by = "Start")
 
 glimpse(complete_data)
 View(complete_data)
+
+sum(complete.cases(complete_data))
