345a0c5e2c0a6ac628114e65c50ea236be913cdb
filipskrabak <xskrabakf@stuba.sk>
2026-03-15 22:29:41 +0100

    refactor(structure): adjust project structure, refactor code, add assigment spec

--- project.R
+++ project.R
@@ -1,22 +1,12 @@
 library(tidyverse)
 library(arrow)
 library(lubridate)
+library(here)
 
-folder_path <- "C:/Users/Filip/Downloads/ParquetFiles(2)/E2a"
-
-file_list <- list.files(path = folder_path, pattern = "\\.parquet$", full.names = TRUE)
-
-raw_air_data <- file_list %>%
-  map_dfr(read_parquet)
-
-View(raw_air_data)
-
-clean_air_data <- raw_air_data %>%
-  select(Start, Pollutant, Value, Samplingpoint) %>%
-  
-  mutate(Start = ymd_hms(Start)) %>%
-
-  # Translate EU codes 
+clean_air_data <- open_dataset(here("pollutants")) %>%
+  select(Start, Pollutant, Value, Samplingpoint) %>% # lazy loading
+  collect() %>%
+  # Translate EU codes into factors
   mutate(Pollutant_Name = case_when(
     Pollutant == 1 ~ "SO2",
     Pollutant == 5 ~ "PM10",
@@ -26,39 +16,31 @@ clean_air_data <- raw_air_data %>%
     Pollutant == 6001 ~ "PM2.5",
     TRUE ~ as.character(Pollutant)
   )) %>%
-  
   select(Start, Pollutant_Name, Value) %>%
-  
   group_by(Start, Pollutant_Name) %>%
   # This takes the average of multiple stations! TODO: fix
-  summarise(Value = mean(Value, na.rm = TRUE)) %>%
-  
+  summarise(Value = mean(Value, na.rm = TRUE), .groups = "drop") %>%
   pivot_wider(
     names_from = Pollutant_Name, 
     values_from = Value
   ) %>%
-  
   arrange(Start)
 
 glimpse(clean_air_data)
 
 View(clean_air_data)
 
-
 ## Weather
 
-weather_file_path <- "C:/Users/Filip/Downloads/ParquetFiles(2)/E2a/open-meteo-48.12N17.10E163m.csv" 
+weather_file_path <- here("weather", "open-meteo-48.12N17.10E163m.csv")
 
 weather_data <- read_csv(weather_file_path, skip = 3) %>%
-  
-  mutate(Start = ymd_hm(time)) %>%
-  
   rename(
+    Start = time,
     Temperature = `temperature_2m (°C)`,
     Rain = `rain (mm)`,
     Wind_Speed = `wind_speed_10m (km/h)`
   ) %>%
-  
   select(Start, Temperature, Rain, Wind_Speed)
 
 complete_data <- clean_air_data %>%
